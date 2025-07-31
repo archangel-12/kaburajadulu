49f162d7a12b1e3bb2c896e58c5f977e0cca977b
0xrsydn <rafayrussani@gmail.com>
2025-07-31 10:14:18 +0700

    migrate to shadcn

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -14,15 +14,13 @@ const badgeVariants = cva(
         secondary:
           "border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
         destructive:
-          "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
+          "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
-        primary:
-          "border-transparent bg-primary/10 text-primary w-[52px] h-[28px] rounded-lg gap-2 p-[2px_8px]",
       },
     },
     defaultVariants: {
-      variant: "primary",
+      variant: "default",
     },
   }
 )
